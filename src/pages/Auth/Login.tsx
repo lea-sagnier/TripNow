569f6e02467073335a5844d52be9fc7674553e7d
Léa SAGNIER <l.sagnier@sumit.fr>
2023-06-16 11:33:51 +0200

    modif login

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -12,8 +12,6 @@ import { Link } from "react-router-dom";
 import {
   signInWithEmailAndPassword,
   GoogleAuthProvider,
-  signInWithPopup,
-  signInWithRedirect,
 } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
 import { eyeOffOutline, eyeOutline, mailOutline } from "ionicons/icons";
@@ -25,7 +23,6 @@ const Login: React.FC = () => {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const providerGoogle = new GoogleAuthProvider();
   const [loading, setLoading] = useState(true);
 
   function logIn() {
@@ -37,10 +34,6 @@ const Login: React.FC = () => {
         console.error(error);
         setErrorMessage("Adresse e-mail ou mot de passe incorrect");
       });
-  }
-
-  function signInWithGoogle() {
-    signInWithRedirect(auth, providerGoogle);
   }
 
   useEffect(() => {
@@ -72,7 +65,7 @@ const Login: React.FC = () => {
                     <IonInput
                       name="email"
                       placeholder="exemple@gmail.com"
-                      onIonChange={(e: any) => setEmail(e.target.value)}
+                      onIonInput={(e: any) => setEmail(e.target.value)}
                     />
                     <IonIcon
                       className="password-toggle-icon"
@@ -85,7 +78,7 @@ const Login: React.FC = () => {
                       name="password"
                       type={showPassword ? "text" : "password"}
                       placeholder="Mot de passe"
-                      onIonChange={(e: any) => setPassword(e.target.value)}
+                      onIonInput={(e: any) => setPassword(e.target.value)}
                     />
                     <IonIcon
                       className="password-toggle-icon"
