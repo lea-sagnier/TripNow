f34011c20bfe80808df5f955e12d8daf4a97ab72
Léa SAGNIER <l.sagnier@sumit.fr>
2023-06-16 11:33:41 +0200

    verif mail

--- src/pages/Auth/VerifyEmail.tsx
+++ src/pages/Auth/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonContent, IonHeader, IonPage } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import { sendEmailVerification } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "../../firebaseConfig";
@@ -6,66 +6,80 @@ import { useHistory } from "react-router-dom";
 import { useCurrentUser } from "../../hooks/UserHook";
 
 export const VerifyEmail = () => {
-    const history = useHistory();
-    const currentUser = useCurrentUser()
-    const [time, setTime] = useState(60)
-    const [timeActive, setTimeActive] = useState(true)
+  const history = useHistory();
+  const currentUser = useCurrentUser();
+  const [time, setTime] = useState(60);
+  const [timeActive, setTimeActive] = useState(true);
+  const [clickEmail, setCLickEmail] = useState(false);
 
-    useEffect(() => {
-        let interval : any = null
-        if(timeActive && time !== 0 ){
-          interval = setInterval(() => {
-            setTime((time) => time - 1)
-          }, 1000)
-        }else if(time === 0){
-          setTimeActive(false)
-          setTime(60)
-          clearInterval(interval)
-        }
-        return () => clearInterval(interval);
-      }, [timeActive, time])
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-          currentUser?.reload()
-          .then(() => {
-            if(currentUser?.emailVerified){
-              clearInterval(interval)
-              history.push('/home')
-            }
-          })
-          .catch((err) => {
-            alert(err.message)
-          })
-        }, 1000)
-      }, [history, currentUser])  
-    
-    const resendEmailVerification = () => {
-        if(auth.currentUser !== null){
-            sendEmailVerification(auth.currentUser)
-            .then(() => {
-                setTimeActive(true)
-            }).catch((err) => {
-            alert(err.message)
-            })
-        }
+  useEffect(() => {
+    let interval: any = null;
+    if (timeActive && time !== 0) {
+      interval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    } else if (time === 0) {
+      setTimeActive(false);
+      setTime(60);
+      clearInterval(interval);
     }
+    return () => clearInterval(interval);
+  }, [timeActive, time]);
 
-    return (
-        <IonPage>
-          <IonHeader></IonHeader>
-          <IonContent>
-            <section>
-            <h1>Vérifions votre Email.</h1>
-            <p>{currentUser?.email}</p>
-            <IonButton 
-                onClick={resendEmailVerification}
-                disabled={timeActive}
-            >
-                Renvoyer le mail {timeActive && time}
+  useEffect(() => {
+    const interval = setInterval(() => {
+      currentUser
+        ?.reload()
+        .then(() => {
+          if (currentUser?.emailVerified) {
+            clearInterval(interval);
+            history.push("/home");
+          }
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }, 1000);
+  }, [history, currentUser]);
+
+  const resendEmailVerification = () => {
+    if (auth.currentUser !== null) {
+      sendEmailVerification(auth.currentUser)
+        .then(() => {
+          setTimeActive(true);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+    setCLickEmail(true);
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>verification email</IonTitle>
+        </IonToolbar>
+      </IonHeader>      
+      <IonContent>
+        <section>
+          <h1>Avant tout, voulez vous vérifiez votre Email ? </h1>
+          <p>{currentUser?.email}</p>
+          {clickEmail ? (
+            <IonButton onClick={resendEmailVerification} disabled={timeActive}>
+              Renvoyer le mail {timeActive && time}
             </IonButton>
-            </section>
-          </IonContent>
-        </IonPage>
-      );
-}
+          ) : (
+            <IonButton onClick={resendEmailVerification} >
+              Envoyer le mail
+            </IonButton>
+          )}
+          <IonButton fill="clear" href="./home">
+            Passer
+          </IonButton>
+        </section>
+      </IonContent>
+    </IonPage>
+  );
+};
