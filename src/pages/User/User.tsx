19dd087c97a735b34effd55a60124ba7aa7673f5
Léa SAGNIER <l.sagnier@sumit.fr>
2023-06-18 20:28:02 +0200

    modif page user

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -7,6 +7,7 @@ import {
   IonToolbar,
   IonInput,
   IonText,
+  IonIcon,
 } from "@ionic/react";
 import "../style.css";
 import { auth } from "../../firebaseConfig";
@@ -15,14 +16,25 @@ import UserEmail from "../../components/UserEmail";
 import { updateProfile } from "firebase/auth";
 import { useCurrentUser } from "../../hooks/UserHook";
 import { useEffect, useState } from "react";
+import {
+  starOutline,
+  heartOutline,
+  personCircleOutline,
+  helpCircleOutline,
+  informationCircleOutline,
+  notificationsOutline,
+  settingsOutline,
+  logOutOutline,
+} from "ionicons/icons";
 
 const User: React.FC = () => {
   const navigate = useHistory();
   const user = useCurrentUser();
   const [displayName, setDisplayName] = useState(user?.displayName);
 
-  useEffect(() => {setDisplayName(user?.displayName)}, [user]);
-
+  useEffect(() => {
+    setDisplayName(user?.displayName);
+  }, [user]);
 
   function updateDisplayName() {
     if (user !== null) {
@@ -57,14 +69,62 @@ const User: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        {/* <UserEmail /> */}
-        <IonText>{displayName}</IonText>
+        <section>
+          <h1>{displayName}</h1>
 
-        <IonInput onIonInput={(e: any) => setDisplayName(e.target.value)} />
-        <IonButton onClick={updateDisplayName}>Modifier mon pseudo</IonButton>
-        
-        <IonButton href="./HistoryPage">Historique</IonButton>
-        <IonButton onClick={logOut}>Se déconnecter</IonButton>
+          {/* <IonInput onIonInput={(e: any) => setDisplayName(e.target.value)} />
+        <IonButton onClick={updateDisplayName}>Modifier mon pseudo</IonButton> */}
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={personCircleOutline}></IonIcon>
+              Informations personelles
+            </span>
+          </IonButton>
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={heartOutline}></IonIcon>
+              Wishlist
+            </span>
+          </IonButton>
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={notificationsOutline}></IonIcon>
+              Notifications
+            </span>
+          </IonButton>
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={settingsOutline}></IonIcon>
+              Paramètres
+            </span>
+          </IonButton>
+          <hr />
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={starOutline}></IonIcon>
+              Noter l'application
+            </span>
+          </IonButton>
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={helpCircleOutline}></IonIcon>
+              Aide
+            </span>
+          </IonButton>
+          <IonButton fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={informationCircleOutline}></IonIcon>
+              Mentions légales
+            </span>
+          </IonButton>
+          <hr />
+          <IonButton onClick={logOut} fill="clear">
+            <span className="button-user">
+              <IonIcon slot="start" icon={logOutOutline}></IonIcon>
+              Déconnexion
+            </span>
+          </IonButton>
+        </section>
       </IonContent>
     </IonPage>
   );
