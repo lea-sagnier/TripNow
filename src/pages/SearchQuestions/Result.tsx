f699514d7d17f6ad18a13d9997eb4d2049376d14
lea-sagnier <75316557+lea-sagnier@users.noreply.github.com>
2023-06-23 11:53:59 +0200

    Feature/26 slider result (#29)
    * image ville
    
    * ajout image jspn
    
    * style result
    
    ---------
    
    Co-authored-by: Léa SAGNIER <l.sagnier@sumit.fr>

--- src/pages/SearchQuestions/Result.tsx
+++ src/pages/SearchQuestions/Result.tsx
@@ -1,4 +1,5 @@
 import { IonContent, IonPage } from "@ionic/react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router";
 
 export const Result = () => {
@@ -8,35 +9,34 @@ export const Result = () => {
     // @ts-expect-error
     const results = location.state.params.result;
 
-        const getResults = () => {
-            const result = []
-            for(let i=0; i<5; i++){
-                result.push(<p key={results[i].ville}>{results[i].ville}</p>)
-            }
-            return result
-        }
+    const getResults = () => {
+      const result = [];
+      for (let i = 0; i < 5; i++) {
+        result.push(
+          <div id={`ville${i}`} key={i}>
+              <img className="result-img" src={results[i].imgResult} alt={`Ville ${i}`} />
+          </div>
+        );
+      }
+      return result;
+    };
 
-        return (
-            <IonPage>
-                <IonContent fullscreen>
-                    <section>
-                    <div>
-                        <h1 className="stepperHeaderTitle">Résultats</h1>
-                        <div className="recapInformations">
-                            {
-                                results.length > 0 ? 
-                                    results.length > 5 ? 
-                                        getResults()
-                                        :
-                                        results.map((city: any) => <p key={city.ville}>{city.ville}</p>) 
-                                    :
-                                    <p>Aucune ville ne correspond à vos recherches</p>
-                            }           
-                        </div>
-                    </div>          
-                    </section>       
-                </IonContent>
-                </IonPage>
-            )
-        }
-}
+    return (
+      <IonPage>
+        <IonContent >
+          <div>
+            {results.length > 0 ? (
+              results.length > 5 ? (
+                getResults()
+              ) : (
+                results.map((city: any) => <p key={city.ville}>{city.ville}</p>)
+              )
+            ) : (
+              <p>Aucune ville ne correspond à vos recherches</p>
+            )}
+          </div>
+        </IonContent>
+      </IonPage>
+    );
+  }
+};
